451cdd5cb8109da8d83f14d28a8233691e798287
Jusong Yu <jusong.yeu@gmail.com>
2022-09-28 02:56:56 +0200

    wrapper fortran works

---
+++ src/potential.jl
@@ -0,0 +1,16 @@
+"""
+Function format from paper: 
+Berghe, G. Vanden, V. Fack, and H. E. De Meyer. Journal of computational and applied mathematics 28 (1989): 391-401.
+"""
+
+"""
+V = u0 / (1+t) + u1 * t / (1+t)^2, 
+where,
+t = exp((r - r0) / a)
+"""
+function woods_saxon(rgrid::Vector{Float64}, u0::Float64, u1::Float64, r0::Float64, a::Float64)
+    t = @. exp((rgrid - r0) / a)
+    v = @. u0 / (1+t) + u1 * t / (1+t)^2 
+
+    v
+end
